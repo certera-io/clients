b6f8c7d8df89a66f20ebc36ee66de8bcf03949f2
certeraio <56412151+certeraio@users.noreply.github.com>
2019-10-30 18:19:12 -0700

    Renaming Certera PS module folder to be able to publish correctly
    Also removed all nuget and packaging since Publish-Module takes care of everything

---
+++ src/PowerShell/Certera/Functions/Get-Key.ps1
@@ -0,0 +1,103 @@
+function Get-Key {
+
+    <#
+    .SYNOPSIS
+    Gets a key from Certera
+	
+	.DESCRIPTION
+    A key can be in DER or PEM format.
+    
+    .OUTPUTS
+    string containing the key in DER (base 64 encoded) or PEM format, or `$null`.
+	
+	.EXAMPLE
+	$derKey = Get-Key -CerteraHost cert.mysite.com -ApiKey 8gpkuxy4a304 -KeyName some-key-name -Format der
+
+	Gets a key in DER format.
+	
+	.EXAMPLE
+	$pemKey = Get-Key -CerteraHost cert.mysite.com -ApiKey 8gpkuxy4a304 -KeyName some-key-name -Format pem
+	
+	Gets a key in PEM format.
+	#>
+	
+    [OutputType([string])]
+	param (	
+		[Parameter(Mandatory = $true)]
+		[string]
+		$CerteraHost,
+		
+		[Parameter(Mandatory = $true)]
+		[string]
+		$ApiKey,
+		
+		[Parameter(Mandatory = $true)]
+		[string]
+		$KeyName,
+		
+		[Parameter(Mandatory = $true)]
+		[ValidateSet("der", "pem")]
+		[string]
+		$Format
+	)
+
+	$IsPSCore = $PSVersionTable['PSEdition'] -eq 'Core'
+	
+	$skipCertCheck = $CerteraHost -eq "localhost"
+	
+	# When connecting to localhost, trust the self-signed cert
+	if ($skipCertCheck -And -Not $IsPSCore) {
+		if (-Not "TrustAllCertsPolicy" -as [type]) {
+			add-type @"
+				using System.Net;
+				using System.Security.Cryptography.X509Certificates;
+				public class TrustAllCertsPolicy : System.Net.ICertificatePolicy {
+					public bool CheckValidationResult(
+						ServicePoint srvPoint, X509Certificate certificate,
+						WebRequest request, int certificateProblem) {
+							return true;
+					}
+				}
+"@
+			[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+		}
+	}
+
+	$params = @{
+		format = "$Format"
+	}
+		
+	$headers = @{
+		apiKey = $ApiKey
+	}
+	
+	Write-Host "Requesting key $KeyName"
+	if ($IsPSCore) {
+		$resp = Invoke-WebRequest https://$CerteraHost/api/key/$KeyName `
+			-SkipCertificateCheck:$skipCertCheck `
+			-Body $params `
+			-Headers $headers `
+			-UseBasicParsing
+	}
+	else {
+		$resp = Invoke-WebRequest https://$CerteraHost/api/key/$KeyName `
+			-Body $params `
+			-Headers $headers `
+			-UseBasicParsing
+	}
+	
+	if ($resp.StatusCode -eq 404) {
+		Write-Host "Key $KeyName not found"
+		return $null
+	}
+	
+	if ($resp.StatusCode -ne 200) {
+		throw "Error: $($resp.StatusCode) $($resp.StatusDescription) $($resp.Content)"
+	}
+	
+	$keyData = $resp.Content
+
+	Write-Host "Key retrieved."
+	
+	return $keyData
+}
--- src/PowerShell/Publish-Certera.ps1
+++ src/PowerShell/Publish-Certera.ps1
@@ -10,19 +10,9 @@ param (
 
 $PSScriptFilePath = Get-Item $MyInvocation.MyCommand.Path
 
-$nuspecPath = Join-Path -Path $PSScriptRoot -ChildPath '.\Certera.nuspec' -Resolve
-$manifestPath = Join-Path -Path $PSScriptRoot -ChildPath '.\Module\Certera.psd1' -Resolve
+$manifestPath = Join-Path -Path $PSScriptRoot -ChildPath '.\Certera\Certera.psd1' -Resolve
 
-$modulePath = Join-Path -Path $PSScriptRoot -ChildPath '.\Module\Certera.psd1' -Resolve
-
-# Modify the Certera.nuspec version
-$nuspec = [xml](Get-Content -Raw -Path $nuspecPath)
-if ($nuspec.package.metadata.version -ne $Version)
-{
-	$nuGetVersion = $Version #-replace '-([A-Z0-9]+)[^A-Z0-9]*(\d+)$','-$1$2'
-	$nuspec.package.metadata.version = $nugetVersion
-	$nuspec.Save($nuspecPath)
-}
+$modulePath = Join-Path -Path $PSScriptRoot -ChildPath ".\Certera" -Resolve
 
 $moduleVersionRegex = 'ModuleVersion\s*=\s*(''|")([^''"])+(''|")' 
 $rawManifest = Get-Content -Raw -Path $manifestPath
@@ -31,9 +21,7 @@ if( $rawManifest -notmatch ('ModuleVersion\s*=\s*(''|"){0}(''|")' -f [regex]::Es
 	$rawManifest = $rawManifest -replace $moduleVersionRegex,('ModuleVersion = ''{0}''' -f $Version)
 	$rawManifest | Set-Content -Path $manifestPath -NoNewline
 }
-	
-.\nuget.exe pack Certera.nuspec
 
-#Publish-Module -Path $ModulePath `
-#			   -Repository 'PSGallery' `
-#			   -NuGetApiKey $ApiKey
+Publish-Module -Path $modulePath `
+			   -Repository 'PSGallery' `
+			   -NuGetApiKey $ApiKey
